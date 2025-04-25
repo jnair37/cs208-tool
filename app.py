a00042fd7a6ad4c8070c804158cb89d5a4248bfc
Jade Nair <jnair@college.harvard.edu>
2025-04-25 15:58:06 -0400

    buttons and static graph

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import numpy as np
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+matplotlib.use('Agg')
 
 app = Flask(__name__)
 
@@ -6,12 +13,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/gen-plot")
+@app.route("/gen_plot")
 def gen_plot():
-    return
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
-def hello():
-    return "Hello, Flask!"
+def create_figure():
+    fig, ax = plt.subplots(figsize=(10, 6))
+    # Find x and y... ?
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+    ax.plot(x, y)
+    return fig
+
+# def hello():
+#     return "Hello, Flask!"
 
 if __name__ == "__main__":
     app.run(debug=True)
