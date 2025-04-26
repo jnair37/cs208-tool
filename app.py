d1f30379a36edf24ecb491a64e73065cbc24b390
Jade Nair <jnair@college.harvard.edu>
2025-04-26 07:21:32 -0400

    plot works

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, jsonify
 import matplotlib.pyplot as plt
 import matplotlib
 import io
@@ -15,19 +15,43 @@ def index():
 
 @app.route("/gen_plot", methods=['GET', 'POST'])
 def gen_plot():
+    data = request.json
+    slope = 1
+    intercept = 1
+    print(request.method)
+    if request.method == 'POST':
+        print("Post???")
+        slope = float(data.get("slope", 1.5))
+        intercept = float(data.get("intercept", 1.5))
     print("HEllo!")
-    fig = create_figure()
+    plot_url = f"/plot.png?slope={slope}&intercept={intercept}"
+    return jsonify({'plot_url': plot_url})
+    
+@app.route('/plot.png')
+def plot_png():
+    slope = float(request.args.get("slope", 1.0))
+    intercept = float(request.args.get("intercept", 1.0))
+    
+    fig = create_figure(slope, intercept)
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
-def create_figure():
+
+def create_figure(slope, intercept):
+    plt.close()
     print("Creating figure")
+    print(slope)
+    print(intercept)
+    plt.figure()
     fig, ax = plt.subplots(figsize=(10, 6))
-    # Find x and y... ?
-    x = np.linspace(0, 10, 100)
-    y = np.sin(x)
-    ax.plot(x, y)
+    #if x_range is None:
+    x_vals = np.array(ax.get_xlim())
+    y_vals = intercept + slope * x_vals
+    ax.plot(x_vals, y_vals, '--')
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_title(f"Line with slope {slope} and intercept {intercept}")
     return fig
 
 # def hello():
