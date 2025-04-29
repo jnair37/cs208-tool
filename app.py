bf13c2e6334fd53449de649ec50cf2d6d3097ef7
Jade Nair <jnair@college.harvard.edu>
2025-04-29 18:53:52 -0400

    fixed users page

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 import io
 import numpy as np
+import math
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 matplotlib.use('Agg')
@@ -28,42 +29,48 @@ def aboutdp():
 @app.route("/gen_plot", methods=['GET', 'POST'])
 def gen_plot():
     data = request.json
-    slope = 1
-    intercept = 1
-    print(request.method)
+    # slope = 1
+    # intercept = 1
+    epsilon = 1
+    # print(request.method)
     if request.method == 'POST':
-        print("Post???")
-        slope = float(data.get("slope", 1.5))
-        intercept = float(data.get("intercept", 1.5))
+        # print("Post???")
+        # slope = float(data.get("slope", 1.5))
+        # intercept = float(data.get("intercept", 1.5))
+        epsilon = float(data.get("epsilon", 1.5))
     print("HEllo!")
-    plot_url = f"/plot.png?slope={slope}&intercept={intercept}"
+    plot_url = f"/plot.png?epsilon={epsilon}"
     return jsonify({'plot_url': plot_url})
     
 @app.route('/plot.png')
 def plot_png():
-    slope = float(request.args.get("slope", 1.0))
-    intercept = float(request.args.get("intercept", 1.0))
+    #slope = float(request.args.get("slope", 1.0))
+    #intercept = float(request.args.get("intercept", 1.0))
+    epsilon = float(request.args.get("epsilon", 1.5))
     
-    fig = create_figure(slope, intercept)
+    fig = create_figure(epsilon)
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
 
-def create_figure(slope, intercept):
+def privacy_risk(epsilon):
+    return 1/(1 + math.e**(-1*epsilon))
+
+def create_figure(epsilon):
     plt.close()
     print("Creating figure")
-    print(slope)
-    print(intercept)
+    print(epsilon)
     plt.figure()
     fig, ax = plt.subplots(figsize=(10, 6))
     #if x_range is None:
-    x_vals = np.array(ax.get_xlim())
-    y_vals = intercept + slope * x_vals
+    x_vals = np.linspace(0, 10, 400)
+    y_vals = privacy_risk(x_vals)
     ax.plot(x_vals, y_vals, '--')
+    ax.plot([epsilon], [privacy_risk(epsilon)], marker='o', color='red', markersize=10)
     ax.set_xlabel("x")
     ax.set_ylabel("y")
-    ax.set_title(f"Line with slope {slope} and intercept {intercept}")
+    ax.set_title(f"Epsilon= {epsilon}; privacy risk={privacy_risk(epsilon)}")
     return fig
 
 # def hello():
